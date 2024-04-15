d0fdf90c45fc51d929b32463db7eabffa637b7b5
prathmesh <iprathmeshsangam@gmail.com>
2024-04-15 20:15:56 +0530

    Methods : Parameter and Multiple parameter

--- src/JavaMethods.java
+++ src/JavaMethods.java
@@ -5,9 +5,18 @@ public class JavaMethods {
     static  void MyMethod(){
         System.out.println("I just got Excuted");
     }
+
+    static void display(){
+        System.out.println("This Method will print something");
+    }
+
+
     public static void main(String[] args) {
         //Code will be executed here
         MyMethod();
+
         // Void mean :- This method does not return anything
     }
+
+
 }
---
+++ src/MoreMethods.java
@@ -0,0 +1,13 @@
+public class MoreMethods {
+
+
+    static  void display(String fname ,int age){
+        System.out.println(fname + " is my first name and " + age  + " is my age");
+    }
+    public static void main(String[] args) {
+        //Code
+        display("prathmesh" , 27);
+        display("neha",28);
+        display("Harshita",21);
+    }
+}
