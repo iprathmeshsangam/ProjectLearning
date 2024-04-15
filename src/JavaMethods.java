adf91cd637a22804bf0fa9d47cd80993f2dcf938
prathmesh <iprathmeshsangam@gmail.com>
2024-04-15 20:07:48 +0530

    This is a Method in Java

---
+++ src/JavaMethods.java
@@ -0,0 +1,13 @@
+//This is a class
+public class JavaMethods {
+    //This is a Method
+
+    static  void MyMethod(){
+        System.out.println("I just got Excuted");
+    }
+    public static void main(String[] args) {
+        //Code will be executed here
+        MyMethod();
+        // Void mean :- This method does not return anything
+    }
+}
