958a57c30640894fa18dc0680c8fb04c5ee84f26
prathmesh <iprathmeshsangam@gmail.com>
2024-04-15 20:01:29 +0530

    Changes made to the class

--- src/Addition.java
+++ src/Addition.java
@@ -1,13 +1,23 @@
+
+//this is a class
 class Main{
+
+
+    //This is a Method
     public static void main(String[] args) {
        int [] nums = {1,2,3,4,5,6,7,8,9,0};
 
        //multi Dimensinal Array
+        int [][] nums2 = {{1,2,3,},{4,5,6}};
+        for (int i = 0 ; i < nums2.length ; i++){
+            for (int j = 0 ; j < nums2[i].length; j++){
+                System.out.println(nums2[i][j]);
+            }
+        }
 
-        int [][] myNumbers  = {{1,2,3} , {4,5,6,}};
-        System.out.println("This is a number" +     " " + myNumbers[1][0]);
-       for(int i : nums){
-           System.out.println(i*2);
-       }
+        String [] names = {"Prathmesh","Pragati","Pratiksha"};
+        for (int i = 0; i < names.length ; i++){
+            System.out.println(names[i]);
+        }
     }
 }
